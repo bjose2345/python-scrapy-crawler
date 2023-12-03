be3ebfadf133ba49044c6f9e99a03e957b9bb24c
bjose2345 <bjose2345@gmail.com>
2023-12-03 20:12:30 +0100

    move export logic to separate folder

---
+++ as_scraper/exports/__init__.py
@@ -0,0 +1,5 @@
+import dotenv
+## default directory for .env file is the current directory
+## if you set .env in different directory, put the directory address load_dotenv("directory_of_.env)
+dotenv.load_dotenv()
+__all__ = ["group_by_platform_id"]
---
+++ as_scraper/exports/group_by_platform_id.py
@@ -0,0 +1,81 @@
+import os
+import re
+from datetime import datetime
+from pathlib import Path
+import pymongo
+
+OUTPUT_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'output')) 
+
+mongodb_uri = os.getenv('MONGODB_URI')
+mongodb_db = os.getenv('MONGODB_DB')
+mongo_username = os.getenv('MONGODB_USERNAME')
+mongo_password = os.getenv('MONGODB_PASSWORD')
+client = pymongo.MongoClient(
+            mongodb_uri, 
+            username=mongo_username, 
+            password=mongo_password
+            )
+db = client[mongodb_db]
+collection_name = "as_items"
+
+def execute():
+
+    exports = db[collection_name].aggregate([
+        {
+            "$unwind": "$details"
+        },
+        {
+            "$unwind": "$details.external_links"
+        },    
+        {
+            "$match": {
+            "product_id": {
+                "$ne": None
+            },
+            "details.stage": None
+            }
+        },
+        {
+            "$group": {
+            "_id": "$product_id",
+            "post_id": {
+                "$push": "$details.post_id"
+            },
+            "external_links": {
+                "$push": "$details.external_links"
+            }
+            }
+        }    
+    ])
+
+    if exports:
+
+        p = Path(__file__).with_name('crawljob.template')
+        today = datetime.today().strftime('%Y%m%d-%H%M%S')
+
+        with p.open('r') as f:
+            template_json = f.read()
+
+        for export in exports:
+            for post_id in export['post_id']:
+                ## update stage to FETCHED status
+                db[collection_name].update_one({'details.post_id': post_id}, {'$set': {'details.$[].stage': 'FETCHED'}}) 
+
+            values = {
+                'text': '[' + ', '.join(str(x) for x in export['external_links']) + ']',
+                'packageName': export['_id'],
+                'comment': 'Created at ' + today
+            }
+
+            after_replace = re.sub('<(.+?) placeholder>', lambda match: values.get(match.group(1)), template_json)
+            output_directory_path = Path(OUTPUT_DIRECTORY)
+            output_directory_path.mkdir(exist_ok=True)
+            filename = export['_id'] + '_' + today + '.crawljob'
+            
+            with open(str(output_directory_path) + os.sep + filename, 'w') as f:
+                f.write(after_replace)
+
+    client.close()
+
+if __name__ == '__main__':
+   execute()
