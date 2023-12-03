ffd62e0e5593a30e34df77b51646fa7c06a9a95c
bjose2345 <bjose2345@gmail.com>
2023-12-04 00:04:46 +0100

    added exporters script dynamically

--- as_scraper/exports/__init__.py
+++ as_scraper/exports/__init__.py
@@ -1,5 +1,11 @@
+import os
 import dotenv
 ## default directory for .env file is the current directory
 ## if you set .env in different directory, put the directory address load_dotenv("directory_of_.env)
 dotenv.load_dotenv()
-__all__ = ["group_by_platform_id"]
+__all__ = []
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+for f in os.listdir(dirname):
+    if f != "__init__.py" and os.path.isfile("%s/%s" % (dirname, f)) and f[-3:] == ".py":
+        __all__.append(f[:-3])
